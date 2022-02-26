106712cc9c253b4a2f228ee9b5f26d77e3a91df7
vikrantShrinkcom <vikrant.shrinkcom@gmail.com>
2022-02-26 12:58:18 +0530

    updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 // import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
@@ -19,7 +20,9 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   // or after 30 seconds (whichever comes first).
   registrationStrategy: 'registerWhenStable:30000'
 })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
+
 export class HomePage {
 
-  constructor() {}
+  myImage = null;
+  position: Position = null;
 
+  constructor() {
+    alert("helloo")
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    this.myImage = image.webPath;
+  }
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here's my current location: 
+        ${this.position.coords.latitude}, 
+        ${this.position.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    });
+  }
 }
+
